204ed13ebf95a3bd70c6f7f6c786f4e064444e3e
tjshbb <scott.t.3042@gmail.com>
2024-11-09 14:06:49 -0600

    refactor: remove dulpicated styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,7 @@
 import { SimpleGrid, Skeleton, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
+import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
 
 const GameGrid = () => {
@@ -16,9 +17,15 @@ const GameGrid = () => {
                 spacing={10}
             >
                 {isLoading &&
-                    skeletons.map(Skeleton => <GameCardSkeleton key={Skeleton} />)}
-                {games.map((game =>
-                    <GameCard key={game.id} game={game} />
+                    skeletons.map(Skeleton => (
+                        <GameCardContainer>
+                            <GameCardSkeleton key={Skeleton} />
+                        </GameCardContainer>
+                    ))}
+                {games.map(game => (
+                    <GameCardContainer>
+                        <GameCard key={game.id} game={game} />
+                    </GameCardContainer>
                 ))}
             </SimpleGrid>
         </>
